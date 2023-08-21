9e8da9617b1407d1e8801b4196ecd28ff05ac682
Edward Leonardo <edwardleonardo14@gmail.com>
2023-08-22 00:28:01 +0800

    Update 1_🏠_Homepage.py
    added back the learn more link

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -44,16 +44,16 @@ st.write("""
 
 """)
 
-# st.write("---")
+st.write("---")
 
-# st.write("""
-# ## Learn More 🤓
-# To Learn More about the Project, Click the links below
+st.write("""
+## Learn More 🤓
+To Learn More about the Project, Click the links below
 
-# 1. [Project Report in PDF](link-to-project-report-pdf)
+1. [Project Report in PDF](https://drive.google.com/file/d/13g4Zuz2PwJAjRlEhrKWDbO7So5PpTrur/view?usp=drive_link)
 
-# 2. [GitHub Repository of the Project](https://github.com/edwardleonardo14/FYP-HeartDiseasePredictor)
-# """)
+2. [GitHub Repository of the Project](https://github.com/edwardleonardo14/FYP-HeartDiseasePredictor)
+""")
 
 st.write("---")
 
